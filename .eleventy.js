cc009c5071ed8ce7cb632f68a804db6954c1c244
Padraig OBrien <padraig@padraigobrien.com>
2020-10-03 21:15:28 +0100

    adding in path for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const readerBar = require('eleventy-plugin-reader-bar')
 
 module.exports = function (config) {
+    pathPrefix: "/padraigobrien.com/"
     config.addPlugin(readerBar)
     config.addPassthroughCopy("./src/css/style.css");
     config.addPassthroughCopy("./src/images");
